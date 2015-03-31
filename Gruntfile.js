a0dccd6fead93d810f1866ac563923d61a81638f
Ando Yasushi <andyjpn@gmail.com>
2015-03-31 19:51:42 +0900

    dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     'connect': {
-      local: {
+      demo: {
         options: {
           port: 8123,
           base: 'app',
@@ -11,31 +11,37 @@ module.exports = function(grunt) {
       }
     },
     'gh-pages': {
-      options: {
-        clone: 'bower_components/three-canvass'
-      },
       src: [
-        'app/**/*',
+        'dist/**/*',
         '!app/bower_components/three-canvass/**/*'
       ]
     },
+    'copy': {
+      bower: {
+        expand: true,
+        cwd: 'app/bower_components',
+        src: ['**'],
+        dest: 'dist'
+      }
+    },
     'replace': {
-      example: {
-        src: ['app/*'],
+      html: {
+        src: ['app/*.html'],
         dest: 'dist/',
         replacements: [{
           from: 'bower_components',
-          to: '..'
+          to: '.'
         }]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-text-replace');
 
-  grunt.registerTask('build',  ['replace']);
+  grunt.registerTask('build',  ['copy', 'replace']);
   grunt.registerTask('deploy', ['gh-pages']);
   grunt.registerTask('server', ['connect']);
 };
