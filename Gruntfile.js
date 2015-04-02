b87accde1b5704eded177c1a5493df13139386c5
Ando Yasushi <andyjpn@gmail.com>
2015-04-02 18:46:47 +0900

    demos and dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,29 +12,50 @@ module.exports = function(grunt) {
     },
     'gh-pages': {
       options: {
-        base: 'dist'
+        base: 'demos'
       },
       src: [
         '**'
       ]
     },
     'copy': {
-      bower: {
+      'demos-bower': {
         expand: true,
         cwd: 'app/bower_components',
         src: ['**'],
-        dest: 'dist'
+        dest: 'demos'
       },
-      images: {
+      'demos-images': {
         expand: true,
         cwd: 'app',
         src: ['images/**'],
+        dest: 'demos'
+      },
+      'dist-bower': {
+        expand: true,
+        cwd: 'app/bower_components',
+        src: [
+          'core-component-page/core-component-page.html',
+          'polymer/layout.html',
+          'polymer/polymer.html',
+          'polymer/polymer.js',
+          'three.js-controls/src/*.js',
+          'threejs/build/three.js'
+        ],
         dest: 'dist'
       }
     },
     'replace': {
-      html: {
+      'demos-html': {
         src: ['app/*.html'],
+        dest: 'demos/',
+        replacements: [{
+          from: 'bower_components',
+          to: '.'
+        }]
+      },
+      'dist-html': {
+        src: ['app/three-canvas.html'],
         dest: 'dist/',
         replacements: [{
           from: 'bower_components',
@@ -57,7 +78,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-release');
 
-  grunt.registerTask('build',  ['copy:bower', 'copy:images', 'replace']);
+  grunt.registerTask('build-demos',  ['copy:demos-bower', 'copy:demos-images', 'replace:demos-html']);
+  grunt.registerTask('build-dist',  ['copy:dist-bower', 'replace:dist-html']);
+  grunt.registerTask('build',  ['build-demos', 'build-dist']);
   grunt.registerTask('deploy', ['gh-pages']);
   grunt.registerTask('server', ['connect']);
 };
